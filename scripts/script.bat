68c60ab58f221e7771f73a0f2e1344d08b92f222
Benjamin Chan <chanb@ohsu.edu>
2017-04-24 09:35:36 -0700

    Initialize master SAS script

---
+++ scripts/script.bat
@@ -0,0 +1,26 @@
+del ..\output\* /q
+del ..\data\processed\* /q
+
+call :execute contentsSourceDatasets
+
+"C:\Program Files\7-Zip\7z.exe" d ..\output.zip ..\output\*
+"C:\Program Files\7-Zip\7z.exe" a ..\output.zip ..\output\*
+"C:\Program Files\7-Zip\7z.exe" l ..\output.zip > ..\output.zip.log
+"C:\Program Files\7-Zip\7z.exe" d ..\data.zip ..\data\processed\*
+"C:\Program Files\7-Zip\7z.exe" a ..\data.zip ..\data\processed\*
+"C:\Program Files\7-Zip\7z.exe" l ..\data.zip > ..\data.zip.log
+
+exit /b
+
+
+:execute
+set exe="C:\Program Files\SASHome\x86\SASFoundation\9.3\sas.exe"
+set cfg="q:\pgms\bchan\sas93_1\S1\sasv9.cfg"
+set ini="U:\studies\AS\pgms\bchan"
+set scr=U:\studies\AS\pgms\bchan\scripts
+set out=U:\studies\AS\pgms\bchan\output
+set f=%1
+%exe% -CONFIG %cfg% -sasinitialfolder %ini% ^
+  -sysin %scr%\%f%.sas ^
+  -log   %out%\%f%.log ^
+  -print %out%\%f%.log 
