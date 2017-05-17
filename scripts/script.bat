56962d7f7747e614345e3dedcfb460e0182877bd
Benjamin Chan <chanb@ohsu.edu>
2017-05-17 10:47:16 -0700

    Use SAS 9.4

--- scripts/script.bat
+++ scripts/script.bat
@@ -15,8 +15,8 @@ exit /b
 
 
 :execute
-set exe="C:\Program Files\SASHome\x86\SASFoundation\9.3\sas.exe"
-set cfg="q:\pgms\bchan\sas93_1\S2\sasv9.cfg"
+set exe="D:\Program Files\SASHome\x86\SASFoundation\9.4\sas.exe"
+set cfg="Y:\SAS94_32\S1\sasv9.cfg"
 set ini="U:\studies\AS\pgms\bchan"
 set scr=U:\studies\AS\pgms\bchan\scripts
 set out=U:\studies\AS\pgms\bchan\output
