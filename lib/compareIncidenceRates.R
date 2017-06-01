a858747e10b8fa1c6259b2a449a30fa5baed7743
Benjamin Chan <chanb@ohsu.edu>
2017-06-01 15:53:59 -0700

    Add incidence rate comparison function

---
+++ lib/compareIncidenceRates.R
@@ -0,0 +1,19 @@
+compareIncidenceRates <- function (db, exp, cat, dis) {
+  require(magrittr)
+  require(dplyr)
+  require(epitools)
+  midp.exact <-
+    df %>% 
+    filter(database == db) %>% 
+    filter(exposure %in% exp) %>% 
+    filter(outcomeCategory == cat) %>% 
+    filter(disease == dis) %>% 
+    select(c(incidence, personYears)) %>% 
+    as.matrix %>% 
+    rate2by2.test %>% 
+    .[["p.value"]] %>% 
+    .[2, 1]
+  data.frame(database = db, outcomeCategory = cat, disease = dis,
+             comparison = sprintf("%s vs %s", exp[1], exp[2]),
+             pValue = as.numeric(midp.exact))
+}
