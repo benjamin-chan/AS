f76a9d552c2d9364c42e376528d6affa58caf0f8
Benjamin Chan <chanb@ohsu.edu>
2017-06-01 16:47:30 -0700

    Output rates

--- lib/compareIncidenceRates.R
+++ lib/compareIncidenceRates.R
@@ -2,7 +2,7 @@ compareIncidenceRates <- function (db, exp, cat, dis) {
   require(magrittr)
   require(dplyr)
   require(epitools)
-  midp.exact <-
+  obj <-
     df %>% 
     filter(grepl(db, database)) %>% 
     filter(grepl(exp[1], exposure) | grepl(exp[2], exposure)) %>% 
@@ -10,12 +10,14 @@ compareIncidenceRates <- function (db, exp, cat, dis) {
     filter(grepl(dis, disease)) %>% 
     select(c(incidence, personYears)) %>% 
     as.matrix %>% 
-    rate2by2.test %>% 
-    .[["p.value"]] %>% 
-    .[2, 1]
+    rate2by2.test
+  pValue <- obj %>% .[["p.value"]] %>% .[2, 1] %>% as.numeric
+  tab <- obj %>% .[["x"]]
   data.frame(database = db, outcomeCategory = cat, disease = dis,
              comparison = sprintf("%s vs %s", exp[1], exp[2]),
-             pValue = as.numeric(midp.exact)) %>% 
+             rate1 = tab[2, 1] / tab[2, 2] * 100,
+             rate2 = tab[1, 1] / tab[1, 2] * 100,
+             pValue = pValue) %>% 
     mutate(database = as.character(database),
            outcomeCategory = as.character(outcomeCategory),
            disease = as.character(disease),
